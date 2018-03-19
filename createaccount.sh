4a4d7a9361d739504a9a66c954835ec6afe545ed
Leonardo Souza <leonardosilva.souza@yahoo.com.br>
2018-03-19 20:17:21 -0300

    Update createaccount.sh

--- createaccount.sh
+++ createaccount.sh
@@ -1,6 +1,6 @@
 # Creating enviromments with folders and creating accounts
 
-if [ -z "$3" ]; then
+if [ "$#"  "$3" ]; then
 	echo "To create: name_company country email_address_to_receive_data"
 	exit 1;
 fi
